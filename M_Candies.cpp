f596ca274140ed91823a9e077924af29eefe66e4
satyamkant <satyamkant2805@gmail.com>
2023-10-03 21:46:08 +0530

    update

---
+++ M_Candies.cpp
@@ -0,0 +1,26 @@
+// Author:- satyam kant//
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+#define int long long
+
+#define endl "\n"
+
+void solve() {}
+
+int32_t main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    // test case...;
+    int t = 1;
+    cin >> t;
+    // test case end...;
+    for (int i = 1; i <= t; i++) {
+        // cout<<"Case #"<<i<<": ";
+        solve();
+    }
+    return 0;
+}
